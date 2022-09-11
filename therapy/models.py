a4622d697be58ae08bab4d3c87aa48fe1ef9510d
Liz-Conway <zil.conway@gmail.com>
2022-09-11 19:27:02 +0100

    FEAT: Add Therapies page
    This page shows a list of the therapies available for booking.

--- therapy/models.py
+++ therapy/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Style(models.Model):
     name = models.CharField(max_length=254)
-    equipment = models.CharField(max_length=254)
+    equipment = models.TextField(max_length=254)
 
     def __str__(self):
         return self.name
@@ -16,14 +16,15 @@ class Therapy(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=3, decimal_places=0)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     course_sessions = models.PositiveIntegerField(
-        editable=False,
         help_text="The recommended number of sessions for this particular therapy.",
     )
-    location = models.CharField(max_length=254)
+    location = models.CharField(max_length=254, null=True, blank=True)
     extra_requirements = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "Therapies"
 
     def __str__(self):
         return self.name
