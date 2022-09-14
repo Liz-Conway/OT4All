5172e9dff23975c1b4ede709e7e387b1d927fce1
Liz-Conway <zil.conway@gmail.com>
2022-09-14 08:18:02 +0100

    Remove Cloudinary test files

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from home.views import HomePage, MeejaPage
+from home.views import HomePage
 
 urlpatterns = [
     path("", HomePage.as_view(), name="home"),
-    path("meeja/", MeejaPage.as_view(), name="meeja"),
 ]
