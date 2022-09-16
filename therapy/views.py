936043001210bfb64f3e1c8dfc947392bcdbbe0d
Liz-Conway <zil.conway@gmail.com>
2022-09-16 10:48:11 +0100

    FEAT: Sort the Therapies on Therapies page
    Sort therapies by :
    	Name, ascending & descending
    	Price, ascending & descending
    	Style, ascending & descending
    Can also reset by choosing the Sort By... option.

--- therapy/views.py
+++ therapy/views.py
@@ -1,6 +1,7 @@
 from django.views.generic.base import TemplateView
 from therapy.models import Therapy
 from django.shortcuts import render, get_object_or_404
+from django.db.models.functions.text import Lower
 
 
 # Create your views here.
@@ -14,8 +15,55 @@ class AllTherapies(TemplateView):
     def get(self, request, *args, **kwargs):
         all_therapies = Therapy.objects.all()
 
+        # Start as none to ensure we don't get an error
+        # when loading the products page without a search term.
+        query = None
+        sort = None
+        direction = None
+
+        # Access URL parameters by checking whether request.GET exists
+        if request.GET:
+            if "sort" in request.GET:
+                sortkey = request.GET["sort"]
+                sort = sortkey
+                if sortkey == "name":
+                    # annotate() allows us to add another field to the
+                    # dataset returned from the database.
+                    # Using the Lower() function on the original "name" field
+                    all_therapies = all_therapies.annotate(
+                        lower_name=Lower("name")
+                    )
+                    # The reason for copying the sort parameter
+                    # into a new variable called sortkey,
+                    # Is because now we've preserved the original field
+                    # we want to sort on ("name").
+                    # But we have the actual field we're going to sort on,
+                    # ("lower_name") in the sort key variable.
+                    # If we had just renamed sort itself to "lower_name"
+                    # we would have lost the original field ("name")
+
+                    # set the sortKey to lower_name
+                    sortkey = "lower_name"
+
+                if sortkey == "category":
+                    sortkey = "category__name"
+
+                if "direction" in request.GET:
+                    direction = request.GET["direction"]
+                    if direction == "desc":
+                        # Add a minus in front of the sort key
+                        # using string formatting, which reverses the order
+                        sortkey = f"-{sortkey}"
+
+                all_therapies = all_therapies.order_by(sortkey)
+
+        # If there is no sorting
+        # The value of this variable will be the string "None_None".
+        current_sorting = f"{sort}_{direction}"
+
         context = {
             "therapies": all_therapies,
+            "current_sorting": current_sorting,
         }
 
         return render(request, self.template_name, context)
