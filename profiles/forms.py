03882bb63e7171c058a6697928a6bb9289139c39
Liz-Conway <zil.conway@gmail.com>
2022-09-21 20:02:52 +0100

    STYLE: Layout the profile section of My Profile
    On the My Profile page there are 2 parts.
    The first page is the profile information.  This part has been styled.

--- profiles/forms.py
+++ profiles/forms.py
@@ -10,16 +10,17 @@ class UserProfileForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         """
-        Add placeholders and classes, remove auto-generated
-        labels and set autofocus on the first field
+        Add placeholders and classes,
+        and set autofocus on the first field
         """
         # Call the super init() method
         # to set the form up as it would be by default
         super().__init__(*args, **kwargs)
         # A dictionary of placeholders which will show up
-        # in the form fields rather than having
-        # clunky looking labels and empty text boxes.
+        # in the form fields when a default has not been set
         placeholders = {
+            "default_full_name": "Your First name and Surname",
+            "default_email": "Email address",
             "default_phone_number": "Phone number",
             "default_postcode": "Postal Code",
             "default_city": "Town or City",
@@ -28,28 +29,20 @@ class UserProfileForm(forms.ModelForm):
             "default_county": "County, State or Locality",
         }
 
-        # Set the "autofocus" attribute on the default_phone_number field to True
-        # so the cursor will start in the default_phone_number field
+        # Set the "autofocus" attribute on the default_full_namefield to True
+        # so the cursor will start in the default_full_name field
         # when the user loads the page
-        self.fields["default_phone_number"].widget.attrs["autofocus"] = True
+        self.fields["default_full_name"].widget.attrs["autofocus"] = True
 
         # Iterate through the forms fields
         for field in self.fields:
 
             if field != "default_country":
-                if self.fields[field].required:
-                    # Add a star to the placeholder
-                    # if it's a "required" field on the model.
-                    placeholder = f"{placeholders[field]} *"
-                else:
-                    placeholder = placeholders[field]
-
                 # Set all the placeholder attributes
                 # to their values in the dictionary above.
-                self.fields[field].widget.attrs["placeholder"] = placeholder
+                self.fields[field].widget.attrs["placeholder"] = placeholders[
+                    field
+                ]
 
             # Add a CSS class to style each field.
-            self.fields[field].widget.attrs["class"] = "profileFormInput"
-            # Remove the form fields' labels
-            # since we won't need them given the placeholders are now set
-            self.fields[field].label = False
+            self.fields[field].widget.attrs["class"] = "formInput"
--- profiles/models.py
+++ profiles/models.py
@@ -17,28 +17,31 @@ class UserProfile(models.Model):
     # And each profile can only be attached to one use
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-    # Rest of the fields in this model are the address fields we want the
+    # Rest of the fields in this model are the client fields we want the
     # user to be able to provide defaults for.
     # These can come directly from the order model
 
-    # Want all these fields to be optional - null = True and blank = True for each of them
+    # Want all these fields to be optional -
+    # null = True and blank = True for each of them
+    default_full_name = models.CharField(max_length=50, null=True, blank=True)
+    default_email = models.CharField(max_length=254, null=True, blank=True)
     default_phone_number = models.CharField(
         max_length=20, null=True, blank=True
     )
-    # blank_label - use "Country" with the star to indicate it's a required field
-    # since select boxes don't have a placeholder
-    default_country = CountryField(
-        blank_label="Country *", null=True, blank=True
-    )
-    default_postcode = models.CharField(max_length=20, null=True, blank=True)
-    default_city = models.CharField(max_length=40, null=True, blank=True)
     default_street_address1 = models.CharField(
         max_length=80, null=True, blank=True
     )
     default_street_address2 = models.CharField(
         max_length=80, null=True, blank=True
     )
+    default_city = models.CharField(max_length=40, null=True, blank=True)
+    default_postcode = models.CharField(max_length=20, null=True, blank=True)
     default_county = models.CharField(max_length=80, null=True, blank=True)
+    # blank_label - use "Country" with the star to indicate it's a required field
+    # since select boxes don't have a placeholder
+    default_country = CountryField(
+        blank_label="Country *", null=True, blank=True
+    )
 
     def __str__(self):
         return self.user.username
