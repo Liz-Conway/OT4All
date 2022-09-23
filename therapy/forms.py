f9bfe181a1312afedae70c2eeb56e3b76f18beac
Liz-Conway <zil.conway@gmail.com>
2022-09-23 20:34:11 +0100

    STYLE: Layout the "Select Image" button
    On the "Add Therapy" page, style a "Select Image" button in the
     background.
    Place the select image input box on top of it but make it invisible.
    When you click on the input box, it looks like you are clicking the
    background button.

--- therapy/forms.py
+++ therapy/forms.py
@@ -10,18 +10,17 @@ class TherapyForm(forms.ModelForm):
         # Special dunder string called __all__ which will include all the fields
         fields = "__all__"
 
-        # Replace the image field on the form with the custom one which utilises the widget
-        image = forms.ImageField(
-            label="Image", required=False, widget=CustomClearableFileInput
-        )
-        # Override the __init__ method to make a couple changes to the fields
-        def __init__(self, *args, **kwargs):
-            super.__init__(*args, **kwargs)
-            # Get all styles
-            styles = Style.objects.all()
+    # Replace the image field on the form with the custom one which utilises the widget
+    image = forms.ImageField(
+        label="Image", required=False, widget=CustomClearableFileInput
+    )
+    # Override the __init__ method to make a couple changes to the fields
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Get all styles
+        styles = Style.objects.all()
 
-            # iterate through the rest of these fields
-            for field_name, field in self.fields.items():
-                # Set some classes on them to make them match the theme of the rest of our store
-                field.widget.attrs["class"] = "formInput"
-                # field.widget.attrs.update({'class': 'formInput'})
+        # iterate through the rest of these fields
+        for field_name, field in self.fields.items():
+            # Set some classes on them to make them match the theme of the rest of our store
+            field.widget.attrs["class"] = "formInput"
