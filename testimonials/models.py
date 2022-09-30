b5488cadf67edd939c762a7967098a08e9784d3b
Liz-Conway <zil.conway@gmail.com>
2022-09-30 12:40:46 +0100

    FIX: Remove blank default from testimonial field
    Having a default on the testimonial field meant that the database would
     accept a testimonial without any testimonial text, replacing it with
     the default, which in this case was a blank string.
    This default was removed.

--- testimonials/models.py
+++ testimonials/models.py
@@ -7,7 +7,7 @@ class Testimonial(models.Model):
     last_name = models.CharField(max_length=25, null=False, blank=False)
     image = models.ImageField(upload_to="ot4u", null=True, blank=True)
     testimony_title = models.CharField(max_length=60, null=True, blank=True)
-    testimony = models.TextField(null=False, blank=False, default="")
+    testimony = models.TextField(null=False, blank=False)
     # Use "auto_now_add" attribute on the date field
     # which will automatically set the testimonial date and time
     # whenever a new testimonial is created
